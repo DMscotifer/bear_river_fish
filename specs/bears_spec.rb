52b3d46d27fefbbe070a6eb33062f8c0fb0bbe98
Scott Stevenson <scotifer92@googlemail.com>
2018-04-03 15:53:52 +0100

    added test_add_fish_to_bear__adding_fish

--- specs/bears_spec.rb
+++ specs/bears_spec.rb
@@ -6,9 +6,12 @@ require_relative("../bears.rb")
 class BearTest < MiniTest::Test
 
   def setup()
-    @bear = ("Yogi", [])
+    @bear1 = ("Yogi", [])
 
   end
 
+def test_add_fish_to_bear__adding_fish
+  assert_equals(["Dory"], @bear1.add_fish_to_bear("Dory"))
+end
 
 end
--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -5,4 +5,9 @@ require_relative("../fish.rb")
 
 class FishTest < MiniTest::Test
 
+  def setup()
+    @fish1 = ("Dory")
+
+  end
+
 end
